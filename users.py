1c8771bb51d2f6c04adaa26b2513b6d0e9d8a928
ROMAN <42304170+protoroma@users.noreply.github.com>
2020-07-22 11:05:22 +0300

    Add files via upload

---
+++ users.py
@@ -0,0 +1,63 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DB_PATH = "sqlite:///sochi_athletes.sqlite3"
+Base = declarative_base()
+
+class User(Base):
+    """
+    Описывает структуру таблицы user для хранения регистрационных данных пользователей
+    """
+    __tablename__ = 'user'
+    
+    id = sa.Column(sa.INTEGER, primary_key=True)
+    first_name = sa.Column(sa.TEXT)
+    last_name = sa.Column(sa.TEXT)
+    gender = sa.Column(sa.TEXT)
+    email = sa.Column(sa.TEXT)
+    birthdate = sa.Column(sa.TEXT)
+    height = sa.Column(sa.REAL)
+
+def connect_db():
+    """
+    Устанавливает соединение к базе данных, создает таблицы, если их еще нет и возвращает объект сессии 
+    """
+    engine = sa.create_engine(DB_PATH)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(engine)
+    return session()
+
+def request_data():
+    """
+    Запрашивает у пользователя данные и добавляет их в список users
+    """
+    print("Привет! Я запишу твои данные!")
+    first_name = input("Введи своё имя: ")
+    last_name = input("А теперь фамилию: ")
+    gender = input("Какого ты пола? (Male/Female) ")
+    email = input("Адрес твоей электронной почты: ")
+    birthdate = input("Введите дату своего рождения в формате YYYY-MM-DD: ")
+    height = input("Введите свой рост в формате 1, 1.5 или 1.70: ")
+    user = User(
+        first_name=first_name,
+        last_name=last_name,
+        gender=gender,
+        email=email,
+        birthdate=birthdate,
+        height=height
+    )
+    return user    
+
+def main():
+    """
+    Осуществляет взаимодействие с пользователем, обрабатывает пользовательский ввод
+    """
+    session = connect_db()
+    user = request_data()
+    session.add(user)
+    session.commit()
+    print("Спасибо, данные сохранены!")
+
+if __name__ == "__main__":
+    main()
